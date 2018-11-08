28775b4721ec13bf9c0d27704cbd46edc201be62
filhaDeHades <43479786+filhaDeHades@users.noreply.github.com>
2018-11-07 23:44:37 -0200

    Update 0. main.c

--- 0. main.c
+++ 0. main.c
@@ -1,15 +1,27 @@
 #include "2. etd.h"
 #include "1. ng.h"
-
 int main(void){
 	ETD *carc1 = inicializacaoCarac(1);
 	ETD *carc2 = inicializacaoCarac(2);
+	CRTR *inv1, *inv2;
+
 
 	printf("\nNum1\n");
 	escreveCarac(carc1);
 	printf("\nNum2:\n");
 	escreveCarac(carc2);
 
+	inv1 = invC(carc1->prox);
+	printf("\nNum1\n");
+	escreveAlg(inv1);
+	inv2 = invC(carc2->prox);
+	printf("\nNum2:\n");
+	escreveAlg(inv2);
+
+	liberaCarac(carc1);
+	liberaCarac(carc2);
+	liberaAlg(inv1);
+	liberaAlg(inv2);
 
 	/*NG* num1 = inicializacao(num1, 1);
 	NG* num2 = inicializacao(num2, 2);
