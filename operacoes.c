ac6091239024311084fe1ba6ff816b9ad091f369
nicolesuet <nicole-db2011@hotmail.com>
2018-11-25 22:43:29 -0200

    multiplicação

--- operacoes.c
+++ operacoes.c
@@ -94,6 +94,7 @@ ETD* divide(ETD* num1, ETD* num2){
 		maior = maiorMagnitude(somar, n1);
 		if (maior==1) break;
 		contador = soma(contador, somador);
+		escreveCarac(contador);
 
 	}while(maior==2);
 
@@ -261,7 +262,7 @@ ETD* subtrai(ETD* num1, ETD* num2){
 	return resp;
 }
 
-ETD * multiplica(ETD* num1, ETD* num2){
+ETD * multiplica2(ETD* num1, ETD* num2){
 	ETD* n1 = num1, *n2 = num2;
 	ETD* resp = (ETD*)malloc(sizeof(ETD)); //criando lista que retorna o resultado
 	resp->prox = NULL;
@@ -288,11 +289,13 @@ ETD * multiplica(ETD* num1, ETD* num2){
 	int maior = maiorMagnitude(contador, n2);
 	if(maior==0){
 		resp = insereComeco(resp, 0);
+		resp->valor = '+';
 		return resp;
 	}
 	maior = maiorMagnitude(contador, n1);
 	if(maior==0){
 		resp = insereComeco(resp, 0);
+		resp->valor = '+';
 		return resp;
 	}
 
@@ -303,6 +306,107 @@ ETD * multiplica(ETD* num1, ETD* num2){
 	}while(maior!=0);
 
 	resp->prox = somar->prox;
+
+	return resp;
+}
+
+ETD* multiplica(ETD* num1, ETD* num2){
+	int maior =  maiorMagnitude(num1, num2), CaracsNumb2 = 0, CaracsNumb1 = 0;
+	int a, b, num = 0, carry = 0;
+
+	ETD* numb1 = num1, *numb2 = num2; // para não modificar os valores iniciais
+	ETD* resp = (ETD*)malloc(sizeof(ETD)); // lista resultante
+	resp->prox = NULL;
+	resp->ult = NULL;
+
+	if ((num1->prox == NULL) || (num2->prox == NULL)){ // um ou mais deles é contido apenas por zero
+		resp = insereComeco(resp, num);
+		return resp;
+	}
+
+	if (maior == -1){
+		printf("erro ao comparar magnitudes\n");
+		return resp;
+	}
+
+	if (((num1->valor == '-') && (num2->valor == '-')) || // -num1 * -num2
+		((num1->valor == '+') && (num2->valor == '+'))) // num1 *num2
+		resp->valor = '+';
+	else resp->valor = '-';
+
+	CRTR* aux = numb2->prox; // aponta por primeiro caracter da lista
+	while (aux != NULL){
+		CaracsNumb2 ++; // conta o número de caracteres de numb2
+		aux = aux->prox;
+	}
+
+	aux = numb1->prox; // aponta por pirmeiro caracter da lista
+	while (aux != NULL){
+		CaracsNumb1 ++; // conta o número de caracteres de numb1
+		aux = aux->prox;
+	}
+
+	if (CaracsNumb2 > CaracsNumb1){
+		numb1 = num2;
+		numb2 = num1; // multiplicar pelo número com menos caracteres
+		int temp = CaracsNumb1;
+		CaracsNumb1 = CaracsNumb2;
+		CaracsNumb2 = temp; // manter o valor atrelado ao numb correto
+	}
+
+	CRTR* n1 = numb1->ult, *n2 = numb2->ult;
+
+	for (a = 0; a <= CaracsNumb1; a++){ // inserindo a multiplicação da ultima casa de numb2
+		if (a == CaracsNumb1){ // ultima inserção de caractere, o ultimo carry
+			num = carry;
+			resp = insereComeco(resp, num);
+			break;
+		}
+
+		num = n1->car * n2->car + carry;
+
+		carry = num / 10; // carrega o decimal para o proximo num
+		num = num % 10;
+
+		resp = insereComeco(resp, num); // cria os ultimos caracteres de resp, os outros serão adicionados com as proximas multiplicações, os atuais (menos o ultimo) serão modificados também
+		n1 = n1->ant;
+	}
+
+	n1 = numb1->ult; // reseta a posição de n1 para as multiplicações futuras
+	n2 = n2->ant; // anda para a penultima casa de numb2
+	carry = 0;
+	CRTR* p = resp->ult->ant, *q = p; // *p pega o antepenúltimo, *q vai pegar o último de cada produto de num1 * carac de num2
+
+	for (a = 1; a < CaracsNumb2; a++){ // andar o número de casas de numb2, menos a ultima, porque ja foi
+		printf("bbbb\n");
+		for (b = 0; b <= CaracsNumb1; b++){ // a casa representada por a vai ser multiplicada por tds as casas de numb1, precisa ser <= porque a casa adicional será do carry
+			if (b == CaracsNumb1){ // inserção unica, apenas do carry
+				// if (carry == 0) break; // se o carry for zero, não é necessário inserir (senão o número pode acabar com uma casa contendo apenas um 0)
+				num = carry;
+				resp = insereComeco(resp, num);
+				break;
+			}
+			printf("aa\n" );
+
+			printf("%d\n", p->car);
+
+			num = n1->car * n2->car + carry + p->car;
+
+			carry = num / 10;
+			num = num % 10;
+			p->car = num;
+			n1 = n1->ant;
+			// if(p->ant==NULL)
+				// resp = insereComeco(resp, 0);
+				p = p->ant; // vai andar como um "n1" de resp
+		}
+
+		n1 = numb1->ult; // reseta a posição de n1
+		n2 = n2->ant;
+		carry = 0;
+		q = q->ant; // pega a menor casa de resp que será somada na próxima rodada
+		p = q; // volta pra posição que será somada na proxima rodada (como o n1, só que limitada à ultima casa q sofrerá soma)
+	}
 
 	return resp;
 }
